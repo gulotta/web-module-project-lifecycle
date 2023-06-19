e41158d9d0d857698ad83a2ed0c1fad964b71afb
Michael Gulotta <gulottam@gmail.com>
2023-06-19 11:32:08 -0400

    WIP

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -41,6 +41,19 @@ export default class App extends React.Component {
     })
     .catch(this.setAxiosResponseError)
   }
+
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      this.setState({...this.state, todos: this.state.todos.map(td => {
+        if (td.id !== id) return td
+        return res.data.data
+      })
+    })
+    })
+    .catch(this.setAxiosResponseError)
+  }
+
   componentDidMount() {
     // fetch all todos from server
     this.fetchAllTodos()
@@ -52,7 +65,7 @@ export default class App extends React.Component {
         <h2>Todos:</h2>
         {
         this.state.todos.map(td => {
-          return <div key={td.id}>{td.name}</div>
+          return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? '✅' : ''}</div>
         })
         }
       </div>
